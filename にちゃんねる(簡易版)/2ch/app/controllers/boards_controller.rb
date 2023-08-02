2056cb8e92f1108c572c029ebfa0e0c100910707
kimura <2023lmi-student009@la-study.com>
2023-08-02 21:58:12 +0900

    boardsのdestroyまで進んだ

--- にちゃんねる(簡易版)/2ch/app/controllers/boards_controller.rb
+++ にちゃんねる(簡易版)/2ch/app/controllers/boards_controller.rb
@@ -1,22 +1,54 @@
 class BoardsController < ApplicationController
+  # 掲示板の一覧を表示
   def index
+    @boards = Board.all
   end
 
+  #新しい掲示板のフォーム
   def new
+    @board = Board.new
   end
-
-  def show
-  end
-
-  def edit
-  end
-
-  def destroy
-  end
-
-  def update
-  end
-
+  
+  # 新しい掲示板の設定内容のデータ保存
   def create
+    @board = Board.new(params_board)
+    if @board.save
+      redirect_to boards_path
+    else
+      render :new
+    end
+  end
+
+  #掲示板の内容ページの表示
+  def show
+    @board = Board.find(params[:id])
+  end
+
+  
+  # 掲示板の削除
+  def destroy
+    @board = Board.find(params[:id])
+    @board.destroy
+    redirect_to boards_path
+  end
+  
+  # 掲示板の設定の編集ページの表示
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  # 掲示板の設定の編集内容の適用
+  def update
+    @board = Board.find(params[:id])
+    if @board.update(params_board)
+      redirect_to board_path(@board)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def params_board
+    params.require(:board).permit(:title, :editor)
   end
 end
--- にちゃんねる(簡易版)/2ch/config/routes.rb
+++ にちゃんねる(簡易版)/2ch/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :boards
-  resoutces :comments 
+  resources :comments 
+  root to: "borlds#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
