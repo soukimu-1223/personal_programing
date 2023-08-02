675588225cd166f52c67980d7a147423f5c9154e
kimura <2023lmi-student009@la-study.com>
2023-08-02 13:47:57 +0900

    アプリケーションの作成

---
+++ にちゃんねる(簡易版)/2ch/app/controllers/boards_controller.rb
@@ -0,0 +1,22 @@
+class BoardsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def create
+  end
+end
---
+++ にちゃんねる(簡易版)/2ch/app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+  def edit
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def create
+  end
+end
